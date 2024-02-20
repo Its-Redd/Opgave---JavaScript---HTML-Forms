6f8320416519ab2c607a34d3cc39df3c4358a895
Redd <piihlsonyt@gmail.com>
2024-02-20 11:16:40 +0100

    Refactor form3 in script.js and index.html

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -8,7 +8,7 @@ form1.addEventListener('submit', function (e) {
   let firstName1 = document.querySelector('#firstName1');
   let lastName1 = document.querySelector('#lastName1');
   console.log('Opgave 1:');
-  console.log(firstName1.value + ' ' + lastName1.value);
+  alert(firstName1.value + ' ' + lastName1.value);
   firstName1.value = '';
   lastName1.value = '';
 });
@@ -18,7 +18,20 @@ form2.addEventListener('submit', function (e) {
   let number1 = document.querySelector('#number1');
   let number2 = document.querySelector('#number2');
   console.log('Opgave 2:');
-  console.log(parseInt(number1.value) + parseInt(number2.value));
+  alert(parseInt(number1.value) + parseInt(number2.value));
   number1.value = '';
   number2.value = '';
+});
+
+form3.addEventListener('submit', function (e) {
+  e.preventDefault();
+  let number3 = document.querySelector('#price1');
+  console.log('Opgave 3:');
+  number3 = parseInt(number3.value);
+  let temp = number3 / 4;
+  number3 = number3 + temp;
+  let temp2 = number3 / 10;
+  number3 = number3 - temp2;
+  let result = document.querySelector('#result');
+  result.value = number3;
 });
